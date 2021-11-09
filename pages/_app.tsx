540e8665a68ec318929730780be5d4a2e3271ca5
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-11-09 22:06:48 +0000

    removed import statements from broken app context, added argon2 instead of bcrypt (article said bcrypt isn't as good. Sucuessfully hashed password. Working on payload next with jsonwebtoken

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,9 @@
 import '../styles/globals.css'
-import { useAppContext } from './_context'
 import type { AppProps } from 'next/app'
-import { AppWrapper } from './_context'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AppWrapper>
       <Component {...pageProps} />
-    </AppWrapper>
   );
 }
 
---
+++ pages/api/sign-up.ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse, NextApi } from 'next'
+import { connectToDatabase } from '../../utils/database';
+import * as argon2 from 'argon2';
+// import jwt from 'jsonwebtoken';
+
+
+
+type Data = {
+  user?: object;
+  err?: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { db }  = await connectToDatabase();
+
+  const {email, password }: {email: string, password: string}  = req.body;
+
+  if(!email || !password){
+    res.status(400).json({err:'email and password are required fields'});
+  }
+
+  /**
+   * Checks to see if there is an existing email in the database
+   * Handles existing emails so it would require to user to enter a unique email
+   * there is a mongoDB function for this as well.
+   */
+    // const existingUser: [] = await db.collection('users').find({email}).toArray();
+    // if(existingUser !== []) {
+    //   res.status(401).json({err: 'email already exists' });
+    // }
+    const user = await argon2
+      .hash(password)
+      .then(hashedPassword =>  db.collection('users')
+                                 .insertOne({email, hashedPassword}));
+    res.status(201).json({user});
+}
