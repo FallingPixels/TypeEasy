7dd6faa18f807f2566decb538a878aaf356cf0cd
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-11-13 00:35:14 +0000

    refactored code so it wouldn't run into re-rendering multiple times and crashing app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,33 @@
-import {FC, useEffect} from 'react'
+import React, {FC, useEffect, useState} from 'react'
 import decodeToken from '../utils/decode-token';
 import {useRouter} from 'next/router';
+import SignIn from './signIn';
+
 
 const Home: FC = () => {
     const router = useRouter();
-    useEffect(() => {
+    const [homePage, setHomePage] = useState(true)
+    const checkUser = () => {
       const token = window.sessionStorage.getItem('user-jwt-login');
-      const user = token ? decodeToken(token) : null;
-      if(!user){
+      const user = token ? decodeToken(token) : null
+      if (!user) {
+        setHomePage(false)
         router.push('/signIn');
       }
+    }
+     useEffect(() => {
+      checkUser();
     })
-    return (
+    return (<>
+      {homePage &&
       <div>
         <h1>Home Page Baby</h1>
       </div>
+      }
+      {!homePage &&
+      <SignIn/>
+      }
+      </>
       )
 
 }
