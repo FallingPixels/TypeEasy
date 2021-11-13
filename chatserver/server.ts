eae6cedcdc3b072c8e48278ac07aed37d80a26a4
humbertovnavarro <humberto.navarro.dev@gmail.com>
2021-11-12 16:53:33 -0800

    added socket io boilerplate

--- chatserver/server.ts
+++ chatserver/server.ts
@@ -1,5 +1,7 @@
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
+import lang from './lang.json';
+const sessionStore = new Map();
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
@@ -8,8 +10,55 @@ const io = new Server(httpServer, {
   }
 });
 
-io.on("connection", (socket: Socket) => {
-  console.log("a user connected");
+function getSessions(userID: number): Map {
+  const sessions = new Map();
+  //Query the database, then store return sockets that belong to userID as a map
+  return sessions;
+}
+
+//Insert a  session into the database
+function addSession(socket: AppSocket) {
+  const { userID } = socket.user;
+}
+
+// Delete a session from the database
+function deleteSession(socket: AppSocket) {
+  const { userID } = socket.user;
+
+}
+
+// Translate a string
+async function Translate(content: string): string {
+  return new Promise();
+}
+
+
+
+
+io.on("connection", (ioSocket: Socket) => {
+
+  const socket = ioSocket as AppSocket;
+
+  socket.on('login', (token) => {
+    //Test the users token against the database and set the sockets user object
+    //if authorized, add session to doc store
+    addSession(socket);
+  });
+
+  socket.on('disconnect', (reason) => {
+    deleteSession(socket);
+  });
+
+  socket.on('message', async (message: Message) => {
+    const { sender, recievers, locale, content } = message;
+    message.original = message.content;
+    message.content = await Translate(content);
+    recievers.forEach(reciever: User => {
+      io.sockets.sockets.get(reciever).emit(message);
+    })
+  });
+
 });
+
 
 httpServer.listen(process.env.CHAT_PORT || 8080);
