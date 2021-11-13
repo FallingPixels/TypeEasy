23b55351a98baaf47f8dfca46d000cd267a41e4d
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-11-13 00:37:37 +0000

    removed unneeded import statements, functions, variables, interfaces

--- pages/signIn.tsx
+++ pages/signIn.tsx
@@ -1,18 +1,14 @@
 import AuthForm from './components/authForm'
-import React, { useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import decodeToken from '../utils/decode-token';
 
-
-
-
-
-interface User {
-  credentials: object;
-  token: string;
-}
 const accountExists: string[] = ["Don't have an account?", "Sign-up instead!"]
+
+interface Account {
+  email: string;
+  password: string;
+}
 
 const SignIn = () => {
   const router = useRouter();
@@ -25,11 +21,10 @@ const SignIn = () => {
      const email = target.email.value;
      const password = target.password.value;
 
-     const account = { email, password }
+     const account: Account = { email, password }
      axios.post('/api/sign-in', account)
           .then(res => {
             const { token } = res.data;
-            console.log(res.data);
             window.sessionStorage.setItem('user-jwt-login', token);
             router.push('/');
           })
